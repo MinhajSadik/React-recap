15795264d1ec17521b872646d61b63f700033ec3
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-28 13:52:14 +0600

    reacp file add

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const persons =['Minhaj', 'sunni', 'hamida', 'limisa', 'lamia', 'sammi', 'joynul', 'samia', 'sharmin', 'nishat', 'nishita'];
+  // const person = [{age:23, home: 'deulGram'}, {age:18, home: 'janina'}, ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
+        {
+          persons.map(person => <PersonsName name={person} detail={person.age} />)
+        }
+        
+        
         <Counter></Counter>
-        <Users></Users>
-
+    
+        
       </header>
     </div>
   );
+}
+function PersonsName(props){
+  return(<div>
+    <h1>{props.name}</h1>
+    <h3>{props.detail}</h3>
+  </div>)
+  
+}
+
+function CountDiffrent(props){
+  return (<div>
+    <h2>CountDiff:{props.anotherCount}</h2>
+  </div>)
 }
 function Counter(){
   const [count, setCount] = useState(0)
@@ -23,24 +44,8 @@ function Counter(){
     <button onMouseMove={handler}>Increment</button>
     <br/>
     <button onClick={() => setCount(count-1)}>Decrement</button>
+    <CountDiffrent anotherCount={count - 100}></CountDiffrent>
   </div>);
 }
 
-function Users(){
-  const [users, setUser] = useState([]);
-  useEffect(() =>{
-    fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(res => res.json())
-    .then(data => setUser(data))
-  },[])
-
-  return (<div>
-    <h1>Available Users : {users.length};</h1>
-    <ol>
-      {
-        users.map(user => <li>{user.email}</li>)
-      }
-    </ol>
-  </div>);
-}
 export default App;
